16691d76595a671d43b666ee3fd3720e23ce51f1
Bianca Ávalos <108427943+BRABianca@users.noreply.github.com>
2022-09-21 16:47:27 -0400

    Add files via upload

---
+++ src/robo.js
@@ -0,0 +1,80 @@
+const wppconnect = require('@wppconnect-team/wppconnect');
+const firebasedb = require('./firebase.js');
+
+var userStages = [];
+
+wppconnect.create({
+    session: 'whatsbot',
+    autoClose: false,
+    puppeteerOptions: {args: ['--no-sandbox']}
+})
+     .then((client) =>
+        client.onMessage((message) => {
+            console.log('Mensagem digitada pelo usuário:' + message.body);
+            queryUserByPhone(client, message);
+        }))
+        .catch((error) =>
+            console.log(error));
+
+async function queryUserByPhone(client, message) {
+    let phone = (message.from).replace(/[^\d]+/g, '');
+    let userdata = await firebasedb.queryByPhone(phone);
+    if (userdata == null) {
+        userdata = await saveUser(message);
+    }
+    console.log('Usuário corrente: ' + userdata['id']);
+    stages(client, message, userdata);
+}
+
+async function stages(client, message, userdata) {
+    if (userStages[message.from] == undefined) {
+        sendWppMessage(client, message.from, 'Bem vindo ao Robô de Whatsapp do AppBasicão!');
+    }
+    if (userdata['nome'] == undefined) {
+        if (userStages[message.from] == undefined) {
+            sendWppMessage(client, message.from, 'Digite seu *NOME*:');
+            userStages[message.from] = 'nome';
+        } else {
+            userdata['nome'] = message.body;
+            firebasedb.update(userdata);
+            sendWppMessage(client, message.from, 'Obrigada, ' + message.body);
+            sendWppMessage(client, message.from, 'Digite seu *CPF*:');
+            userStages[message.from] = 'cpf';
+        }
+    } else if (userdata['cpf'] == undefined) {
+        if (userStages[message.from] == undefined) {
+            sendWppMessage(client, message.from, 'Digite seu *CPF*:');
+            userStages[message.from] = 'cpf';
+        } else {
+            userdata['cpf'] = (message.body).replace(/[^\d]+/g, '');
+            firebasedb.update(userdata);
+            sendWppMessage(client, message.from, 'Obrigada por informar seu CPF: ' + message.body);
+            sendWppMessage(client, message.from, 'Fim');
+            userStages[message.from] = 'fim';
+        }
+    } else {
+        if (userStages[message.from] == undefined) {
+            userStages[message.from] = 'fim';
+        }
+        sendWppMessage(client, message.from, 'Fim');
+    }
+}
+
+function sendWppMessage(client, sendTo, text) {
+    client.sendText(sendTo, text)
+        .then((result) => {
+            // console.log('SUCESSO: ', result); 
+        })
+        .catch((erro) => {
+            console.error('ERRO: ', erro);
+        });
+}
+
+async function saveUser(message) {
+    let user = {
+        'pushname': (message['sender']['pushname'] != undefined) ? message['sender']['pushname'] : '',
+        'whatsapp': (message.from).replace(/[^\d]+/g, '')
+    }
+    let newUser = firebasedb.save(user);
+    return newUser;
+}
